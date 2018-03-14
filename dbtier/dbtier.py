8912eebda9b83a1dba863a2ffb48eef40c9132ff
carmelopz <carmelopz@gmail.com>
2018-03-14 18:58:27 +0100

    Added db query conversion to json, html and a Flask server to the Python app

--- dbtier/dbtier.py
+++ dbtier/dbtier.py
@@ -1,3 +1,30 @@
 import rethinkdb as r
+
+
 r.connect( "0.0.0.0", 28020).repl()
-print r.db('supply_chain').table('agents').run()
+
+agentsCursor = r.db('supply_chain').table('agents').run()
+agentsList=list(agentsCursor)
+
+from bson.json_util import dumps
+agentsJson=dumps(agentsList)
+
+from json2html import *
+agentsHtml = json2html.convert(json = agentsJson)
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def dbtier():
+	return (agentsHtml)
+
+app.run(host='0.0.0.0', port=40080)
+
+
+
+
+
+
+
+
